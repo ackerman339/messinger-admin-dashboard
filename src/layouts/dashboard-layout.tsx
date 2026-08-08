0be611baa4bebb44079bcec455fd00755a10cc0d
levi ackerman <leviackermansnk339@gmail.com>
2026-08-08 14:51:31 -0500

    feat: add dashboard layout

---
+++ src/layouts/dashboard-layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+import { Sidebar } from '@components/sidebar';
+
+export function DashboardLayout() {
+  return (
+    <div className='flex min-h-screen bg-background text-foreground'>
+      <Sidebar />
+      <main className='min-w-0 flex-1 overflow-auto p-4'>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
