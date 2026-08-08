f8a221b05d2b0916d22df4ecf1f9dcd1a2389aa2
levi ackerman <leviackermansnk339@gmail.com>
2026-08-08 14:52:46 -0500

    feat: add http client

---
+++ src/http-client.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError } from 'axios';
+
+export const httpClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/${import.meta.env.VITE_API_PREFIX}/${import.meta.env.VITE_API_VERSION}`,
+  withCredentials: true,
+});
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    const status = error.response?.status;
+
+    if (status === 401) {
+      const isOnAuthPage = window.location.pathname === '/sign-in';
+
+      if (!isOnAuthPage) {
+        window.location.href = '/sign-in';
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
